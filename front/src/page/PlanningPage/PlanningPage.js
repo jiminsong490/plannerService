4fb1c9ea76c1e228895683fdced311071916a3a7
Song <misosjm@naver.com>
2022-09-09 17:49:07 +0900

    addPlan 제작

--- front/src/page/PlanningPage/PlanningPage.js
+++ front/src/page/PlanningPage/PlanningPage.js
@@ -1,30 +1,59 @@
 import axios from 'axios'
-import React from 'react'
-import { format, parseISO } from 'date-fns'
+import React, { useState } from 'react'
+import { format } from 'date-fns'
 import { useLocation } from 'react-router-dom'
 
 const PlanningPage = () => {
+    const [start, setStart] = useState('')
+    const [end, setEnd] = useState('')
     const location = useLocation()
     const dateObject = location.state?.date
     const year = format(dateObject, 'yyyy')
     const month = format(dateObject, 'M')
     const day = format(dateObject, 'dd')
-    const a = dateObject.toString()
-    const handleClick = async () => {
-        const a = await axios.post(
-            `${process.env.REACT_APP_WEBSITE_BACK}/addPlans`
+    const handleStartChange = (e) => {
+        e.preventDefault()
+        setStart(e.target.value)
+    }
+    const handleEndChange = (e) => {
+        e.preventDefault()
+        setEnd(e.target.value)
+    }
+
+    const handleSubmit = async () => {
+        const res = await axios.post(
+            `${process.env.REACT_APP_WEBSITE_BACK}/addPlans`,
+            {
+                start: start,
+                end: end,
+            }
         )
     }
 
     return (
         <>
-            {dateObject ? (
+            <header>
                 <h1>
                     {year}년 {month} 월{day}일
                 </h1>
-            ) : (
-                <p>Planning</p>
-            )}
+            </header>
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <input
+                        onChange={handleStartChange}
+                        value={start}
+                        placeholder='시작 시간'
+                    ></input>
+                </div>
+                <div>
+                    <input
+                        onChange={handleEndChange}
+                        value={end}
+                        placeholder='종료 시간'
+                    ></input>
+                </div>
+                <button type='submit'>제출</button>
+            </form>
         </>
     )
 }
